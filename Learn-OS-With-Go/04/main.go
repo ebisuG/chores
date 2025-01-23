b1c7b635f8e01d42e994157eec5d614c46449351
Eiichi Sayama <eiichimouse@outlook.jp>
2025-01-24 00:20:14 +0900

    Read and write png data chunk by chunk

--- Learn-OS-With-Go/04/main.go
+++ Learn-OS-With-Go/04/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"bytes"
 	"encoding/binary"
 	"fmt"
+	"hash/crc32"
 	"io"
 	"os"
 )
@@ -20,6 +22,27 @@ func dumpChunk(chunk io.Reader) {
 	buffer := make([]byte, 4)
 	chunk.Read(buffer)
 	fmt.Printf("chunk '%v' (%d bytes)\n", string(buffer), length)
+	if bytes.Equal(buffer, []byte("tEXt")) {
+		rawText := make([]byte, length)
+		chunk.Read(rawText)
+		fmt.Println(string(rawText))
+	}
+}
+
+func textChunk(text string) io.Reader {
+	byteData := []byte(text)
+	var buffer bytes.Buffer
+	//長さの書き込み
+	binary.Write(&buffer, binary.BigEndian, int32(len(byteData)))
+	//チャンク名の書き込み
+	buffer.WriteString("tEXt")
+	//秘密のデータの書き込み
+	buffer.Write(byteData)
+	//calculate CRC
+	crc := crc32.NewIEEE()
+	io.WriteString(crc, "tEXt")
+	binary.Write(&buffer, binary.BigEndian, crc.Sum32())
+	return &buffer
 }
 
 func readChunks(file *os.File) []io.Reader {
@@ -47,12 +70,29 @@ func readChunks(file *os.File) []io.Reader {
 }
 
 func main() {
-	file, err := os.Open("Lenna.png")
+	file, err := os.Open("Lenna2.png")
 	if err != nil {
 		panic(err)
 	}
+
+	//Put secret string into png file
+	// newFile, err := os.Create("Lenna2.png")
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// defer newFile.Close()
+
 	chunks := readChunks(file)
-	for _, chunk := range chunks {
+
+	// //write signature
+	// io.WriteString(newFile, "\x89PNG\r\n\x1a\n")
+	// // 先頭に必要なIHDRチャンクを書き込み
+	// io.Copy(newFile, chunks[0])
+	// // テキストチャンクを追加
+	// io.Copy(newFile, textChunk("ASCII PROGRAMMING++"))
+
+	for _, chunk := range chunks[1:] {
 		dumpChunk(chunk)
+		// io.Copy(newFile, chunk)
 	}
 }
