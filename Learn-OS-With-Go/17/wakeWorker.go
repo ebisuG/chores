0cc7d82bc85e4630d0928847d54be9cecc409004
Eiichi Sayama <eiichimouse@outlook.jp>
2025-03-08 17:24:17 +0900

    learn parallel processing with sync package

---
+++ Learn-OS-With-Go/17/wakeWorker.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+// cond.Wait()と、cond.Broadcast()はペアで使う
+func main() {
+	var mutex sync.Mutex
+	cond := sync.NewCond(&mutex)
+
+	for _, name := range []string{"A", "B", "C"} {
+		go func(name string) {
+			// ロックしてからWaitメソッドを呼ぶ
+			mutex.Lock()
+			defer mutex.Unlock()
+			// Broadcast()が呼ばれるまで待つ
+			cond.Wait()
+			// 呼ばれた！
+			fmt.Println(name)
+		}(name)
+	}
+
+	fmt.Println("よーい")
+	time.Sleep(time.Second)
+	fmt.Println("どん！")
+	// 待っているgoroutineを一斉に起こす
+	cond.Broadcast()
+	time.Sleep(time.Second)
+}
