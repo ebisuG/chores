3b0b4a1df905d4ac85c78040f6210dd6f6da39d5
Eiichi Sayama <eiichimouse@outlook.jp>
2025-07-13 17:22:23 +0900

    feat:add a page for HelloWorld and HelloWorld/Welcome

---
+++ C_sharp/sample-core-mvc/sample-core-mvc/Controllers/HelloWorldController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers;
+
+//Controller is base class from Microsoft.AspNetCore.Mvc
+public class HelloWorldController : Controller
+{
+    //all public method in Controller class is handled as a callable http endpoint
+    //Index() can be accessed with /HelloWorld
+    public string Index()
+    {
+        return "This is my defaul action...";
+    }
+
+    //Welocme can be accessed with /HelloWorld/Welocme
+    //name and numtimes are query parameter
+    public string Welcome(string name, int numTimes=1)
+    {
+        return HtmlEncoder.Default.Encode($"Hello {name}, Numtime is ; {numTimes}");
+    }
+
+    //program.cs accecpts both Welcome/3?name=Rick&numtimes=4 and Welcome?name=Rick&numtimes=4
+}
