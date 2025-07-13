75e39b58a740c43f9f6ff44e8e801a5b637fa7fd
Eiichi Sayama <eiichimouse@outlook.jp>
2025-07-13 17:52:05 +0900

    feat:Use cshtml file to render View

--- C_sharp/sample-core-mvc/sample-core-mvc/Controllers/HelloWorldController.cs
+++ C_sharp/sample-core-mvc/sample-core-mvc/Controllers/HelloWorldController.cs
@@ -8,9 +8,11 @@ public class HelloWorldController : Controller
 {
     //all public method in Controller class is handled as a callable http endpoint
     //Index() can be accessed with /HelloWorld
-    public string Index()
+    //public string Index()
+    public IActionResult Index()
     {
-        return "This is my defaul action...";
+        //return "This is my defaul action...";
+        return View();  
     }
 
     //Welocme can be accessed with /HelloWorld/Welocme
@@ -18,6 +20,11 @@ public class HelloWorldController : Controller
     public string Welcome(string name, int numTimes=1)
     {
         return HtmlEncoder.Default.Encode($"Hello {name}, Numtime is ; {numTimes}");
+    }
+
+    public ActionResult Welcome2()
+    {
+        return View();
     }
 
     //program.cs accecpts both Welcome/3?name=Rick&numtimes=4 and Welcome?name=Rick&numtimes=4
